0ce3cfc6d8951805c5dd440f4a6e4df189cabec6
Henrique Sander <henrique.sander@i-sensi.io>
2023-10-28 16:56:06 -0300

    create tasks files

---
+++ comedouro-gateway/components/tasks/include/task_pn532.h
@@ -0,0 +1,45 @@
+/**
+ * @file file_name.h
+ * @author Henrique Sander Lourenço
+ * @brief Description.
+ * @version 0.1
+ * @date yyyy-mm-dd
+ *
+ * @copyright Copyright 2023 Henrique Sander Lourenço
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ *
+ *
+ */
+
+#ifndef FILE_H
+#define FILE_H
+
+// >>>>>>>>>>>>>>>>>>>> Common includes
+
+#include "esp_err.h"
+
+// >>>>>>>>>>>>>>>>>>>> App includes
+
+// >>>>>>>>>>>>>>>>>>>> ESP-IDF includes
+
+// >>>>>>>>>>>>>>>>>>>> libc includes
+
+// >>>>>>>>>>>>>>>>>>>> Other includes
+
+// >>>>>>>>>>>>>>>>>>>> Defines
+
+// >>>>>>>>>>>>>>>>>>>> Declarations
+
+#endif
